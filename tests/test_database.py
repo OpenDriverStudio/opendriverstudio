d1de04181772af8573fab37ac25b75663edaae95
rluzuriaga <rodrigoluzuriaga@icloud.com>
2024-11-17 19:18:08 -0800

    test(database): add tests for insert and select, also refactor to conftest

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,52 +1,9 @@
-import tempfile
 from pathlib import Path
 
 import pytest
 
-from src.opendriverstudio.database.database import Database, DatabaseCreationError
-from tests.data_for_tests import WRONG_SYNTAX_SCHEMA
-
-
-TESTING_DB_FILE = Path(__file__).parent / "tests.db"
-
-
-@pytest.fixture
-def empty_db_schema_file():
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    temp_file.close()  # Closing immediately to avoide locked file issues
-
-    temp_file_path = Path(temp_file.name)
-
-    yield temp_file_path
-
-    if temp_file_path.exists():
-        temp_file_path.unlink()
-
-
-@pytest.fixture
-def create_working_database():
-    database_file = TESTING_DB_FILE
-
-    db = Database(db_file=TESTING_DB_FILE)
-
-    yield db
-
-    if database_file.exists():
-        database_file.unlink()
-
-
-@pytest.fixture
-def wrong_syntax_schema_file():
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    temp_file.write(WRONG_SYNTAX_SCHEMA)
-    temp_file.close()
-
-    temp_file_path = Path(temp_file.name)
-
-    yield temp_file_path
-
-    if temp_file_path.exists():
-        temp_file_path.unlink()
+from src.opendriverstudio.database.database import Database, DatabaseCreationError, MissingFieldError
+from tests.conftest import TESTING_DB_FILE
 
 
 class TestDatabase:
@@ -63,3 +20,36 @@ class TestDatabase:
     def test_database_schema_has_wrong_syntax(self, wrong_syntax_schema_file) -> None:
         with pytest.raises(DatabaseCreationError):
             Database(db_file=TESTING_DB_FILE, db_schema=wrong_syntax_schema_file)
+
+    def test_database_insert_into_drivers_table(self, create_working_database, working_drivers_db_data) -> None:
+        db: Database = create_working_database
+
+        for driver in working_drivers_db_data:
+            db.insert_into_drivers_table(driver)
+
+    def test_database_insert_into_drivers_table_with_missing_field(
+        self, create_working_database, broken_drivers_db_data
+    ) -> None:
+        db: Database = create_working_database
+
+        for driver in broken_drivers_db_data:
+            with pytest.raises(MissingFieldError):
+                db.insert_into_drivers_table(driver)
+
+    def test_database_select_all_from_drivers_table(
+        self, setup_database_with_working_data, select_all_drivers_db_data
+    ) -> None:
+        db: Database = setup_database_with_working_data
+
+        db_output = db.select_all_from_drivers_table()
+
+        assert db_output == select_all_drivers_db_data
+
+    def test_database_select_driver_id_from_drivers_with_driver_name(self, setup_database_with_working_data) -> None:
+        db: Database = setup_database_with_working_data
+
+        db_output_1 = db.select_driver_id_from_drivers_with_driver_name("SP142670")
+        db_output_2 = db.select_driver_id_from_drivers_with_driver_name("SP23456")
+
+        assert db_output_1 == 1
+        assert db_output_2 == 2
