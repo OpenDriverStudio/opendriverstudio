e9320973178bf57ac9b138ff5cd554a44220f321
rluzuriaga <rodrigoluzuriaga@icloud.com>
2024-12-16 21:17:56 -0800

    style(tests): add more type definitions

--- typing_defs.py
+++ typing_defs.py
@@ -1,14 +1,18 @@
-from typing import TypedDict
+from typing import Literal, TypedDict
 
 
-class DriverType(TypedDict):
+# Add more literals in the future when more fields are added to the BROKEN_DB_DATA dictionary in conftest.py
+BrokenDBDataTypeKey = Literal["MISSING_FIELD"]
+
+
+class DriverType(TypedDict, total=False):
     driver_name: str
     driver_version: str
     driver_path: str
     driver_type: str
 
 
-class MachineType(TypedDict):
+class MachineType(TypedDict, total=False):
     machine_id: str
     driver_id: int
     machine_model: str
@@ -18,3 +22,12 @@ class MachineType(TypedDict):
 class WorkingDBDataType(TypedDict):
     drivers: list[DriverType]
     machines: list[MachineType]
+
+
+class BrokenDBCategoryType(TypedDict):
+    drivers: list[DriverType]
+    machines: list[MachineType]
+
+
+class BrokenDBDataType(TypedDict):
+    MISSING_FIELD: BrokenDBCategoryType
