cb604a332882b0a8d1b3dd909d2a3dbd8dfe3874
rluzuriaga <rodrigoluzuriaga@icloud.com>
2024-12-16 19:48:28 -0800

    refactor(tests): use new typing definitions & add machines database data for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,55 +5,96 @@ from pathlib import Path
 import pytest
 
 from src.opendriverstudio.database.database import Database
+from typing_defs import DriverType, MachineType, WorkingDBDataType
 
 
 TESTING_DB_FILE = Path(__file__).parent / "tests.db"
 
 WRONG_SYNTAX_SCHEMA = b"CREATE TABLE test (id INTEGER"  # Wrong syntax on purpose
 
-WORKING_DB_DATA: dict[str, list[dict[str, str]]] = {
+WORKING_DB_DATA: WorkingDBDataType = {
     "drivers": [
         {
-            "driver_name": "SP142670",
-            "driver_version": "3.00",
+            "driver_name": "sp142670",
+            "driver_version": "3.00 REV: A PASS: 1",
             "driver_path": "/path/to/driver",
             "driver_type": "driverpack",
         },
         {
-            "driver_name": "SP23456",
-            "driver_version": "1.00",
+            "driver_name": "sp154299",
+            "driver_version": "7.00 REV: A PASS: 1",
             "driver_path": "/path/to/driver/2",
-            "driver_type": "bios",
+            "driver_type": "driverpack",
+        },
+    ],
+    "machines": [
+        {
+            "machine_id": "8848",
+            "driver_id": 1,
+            "machine_model": "HP EliteBook 850 G8 Notebook PC",
+            "machine_manufacturer": "HP",
+        },
+        {
+            "machine_id": "894F",
+            "driver_id": 2,
+            "machine_model": "HP Elite Mini 800 G9 Desktop PC",
+            "machine_manufacturer": "HP",
         },
     ],
 }
 
-BROKEN_DB_DATA: dict[str, dict[str, list[dict[str, str]]]] = {
+BROKEN_DB_DATA: dict[str, dict[str, list[dict[str, str | int]]]] = {
     "MISSING_FIELD": {
         "drivers": [
             {
-                # "driver_name": "SP142670",  # Missing parameter
-                "driver_version": "3.00",
+                # "driver_name": "sp142670",  # Missing parameter
+                "driver_version": "3.00 REV: A PASS: 1",
                 "driver_path": "/path/to/driver",
                 "driver_type": "driverpack",
             },
             {
-                "driver_name": "SP142670",
-                # "driver_version": "3.00",  # Missing parameter
+                "driver_name": "sp142670",
+                # "driver_version": "33.00 REV: A PASS: 1",  # Missing parameter
                 "driver_path": "/path/to/driver",
                 "driver_type": "driverpack",
             },
             {
-                "driver_name": "SP142670",
-                "driver_version": "3.00",
+                "driver_name": "sp142670",
+                "driver_version": "3.00 REV: A PASS: 1",
                 # "driver_path": "/path/to/driver",  # Missing parameter
                 "driver_type": "driverpack",
             },
             {
-                "driver_name": "SP142670",
-                "driver_version": "3.00",
+                "driver_name": "sp142670",
+                "driver_version": "3.00 REV: A PASS: 1",
                 "driver_path": "/path/to/driver",
                 # "driver_type": "driverpack",  # Missing parameter
+            },
+        ],
+        "machines": [
+            {
+                # "machine_id": "8848",  # Missing parameter
+                "driver_id": 1,
+                "machine_model": "HP EliteBook 850 G8 Notebook PC",
+                "machine_manufacturer": "HP",
+            },
+            {
+                "machine_id": "8848",
+                # "driver_id": 1,  # Missing parameter
+                "machine_model": "HP EliteBook 850 G8 Notebook PC",
+                "machine_manufacturer": "HP",
+            },
+            {
+                "machine_id": "8848",
+                "driver_id": 1,
+                # "machine_model": "HP EliteBook 850 G8 Notebook PC",  # Missing parameter
+                "machine_manufacturer": "HP",
+            },
+            {
+                "machine_id": "8848",
+                "driver_id": 1,
+                "machine_model": "HP EliteBook 850 G8 Notebook PC",
+                # "machine_manufacturer": "HP",  # Missing parameter
             },
         ],
     },
@@ -100,13 +141,23 @@ def wrong_syntax_schema_file() -> Generator[Path, None, None]:
 
 
 @pytest.fixture
-def working_drivers_db_data() -> list[dict[str, str]]:
+def working_drivers_db_data() -> list[DriverType]:
     return WORKING_DB_DATA["drivers"]
 
 
+@pytest.fixture
+def working_machines_db_data() -> list[MachineType]:
+    return WORKING_DB_DATA["machines"]
+
+
 @pytest.fixture(params=["MISSING_FIELD"])
-def broken_drivers_db_data(request) -> list[dict[str, str]]:
+def broken_drivers_db_data(request) -> list[dict[str, str | int]]:
     return BROKEN_DB_DATA[request.param]["drivers"]
+
+
+@pytest.fixture(params=["MISSING_FIELD"])
+def broken_machines_db_data(request) -> list[dict[str, str | int]]:
+    return BROKEN_DB_DATA[request.param]["machines"]
 
 
 @pytest.fixture
