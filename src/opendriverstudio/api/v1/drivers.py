20993cdc911a25304117ab75e1c6c211580b691a
rluzuriaga <rodrigoluzuriaga@icloud.com>
2025-01-20 19:36:45 -0800

    fix(api): change api functions to use depends for the database

--- src/opendriverstudio/api/v1/drivers.py
+++ src/opendriverstudio/api/v1/drivers.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, HTTPException
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
 
 from src.opendriverstudio.database.database import Database, DatabaseInsertionError
 
@@ -6,32 +8,39 @@ from src.opendriverstudio.database.database import Database, DatabaseInsertionEr
 router = APIRouter()
 
 
-@router.get("/drivers")
-async def get_all_drivers() -> list[tuple[int, str, str, str, str]]:
-    db: Database = Database()
+def get_database() -> Database:
+    return Database()
 
+
+@router.get("/drivers")
+async def get_all_drivers(db: Annotated[Database, Depends(get_database)]) -> list[tuple[int, str, str, str, str]]:
     return db.select_all_from_drivers_table()
 
 
 @router.get("/drivers/name={driver_name}")
-async def get_driver_id_from_driver_name(driver_name: str) -> dict[str, int | None]:
-    db: Database = Database()
-
+async def get_driver_id_from_driver_name(
+    db: Annotated[Database, Depends(get_database)],
+    driver_name: str,
+) -> dict[str, int | None]:
     driver_id = db.select_driver_id_from_drivers_with_driver_name(driver_name)
 
     return {"driver_id": driver_id}
 
 
 @router.post("/drivers", responses={409: {"description": "Conflict: Database insertion error."}})
-async def add_driver(driver_name: str, driver_version: str, driver_path: str, driver_type: str) -> dict[str, str]:
+async def add_driver(
+    db: Annotated[Database, Depends(get_database)],
+    driver_name: str,
+    driver_version: str,
+    driver_path: str,
+    driver_type: str,
+) -> dict[str, str]:
     rows = {
         "driver_name": driver_name,
         "driver_version": driver_version,
         "driver_path": driver_path,
         "driver_type": driver_type,
     }
-
-    db: Database = Database()
 
     try:
         db.insert_into_drivers_table(rows)
